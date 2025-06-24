27e72bac76d5bb51506c06b649e3a38cb26454f2
Ascyt <contact@ascyt.com>
2025-06-24 11:13:06 +0200

    Shifted numbers by +1 and shifted 0 to the beginning

--- extract_numbers.py
+++ extract_numbers.py
@@ -20,7 +20,7 @@ else:
 
 glyph_count = 10  # For digits 0-9
 glyph_width = width / glyph_count
-digits = [1,2,3,4,5,6,7,8,9,0]  # Output order: 1-9,0
+digits = [0,1,2,3,4,5,6,7,8,9]
 
 # Helper: get bounding box of a path
 def path_bbox(d):
